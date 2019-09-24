8060141e2a53198abfec99f92475877e35bcccb3
rigobertoReyes <rigoberto.reyes16@tectijuana.edu.mx>
2019-09-23 19:05:19 -0700

    Cuentas registradas

--- alumno/admin.py
+++ alumno/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Alumno,Proyecto
 
 # Register your models here.
+admin.site.register(Alumno)
+admin.site.register(Proyecto)
--- superusuario/admin.py
+++ superusuario/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import MyUserManager,User,Carrera
 # Register your models here.
+
+#admin.site.register(MyUserManager)
+admin.site.register(User)
+admin.site.register(Carrera)
+
