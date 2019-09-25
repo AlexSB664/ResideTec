7f248f3de537b0161b0352840a5a7fd92a2b6860
AlexSB664 <alexskullsoft@gmail.com>
2019-09-24 21:54:36 -0700

    config to db mysql

--- ResideTec/settings.py
+++ ResideTec/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'ResideTec.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
