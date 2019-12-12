c724212350d53d1c67dbc1345b8e08c0f7deb0bb
AlexSB664 <alexskullsoft@gmail.com>
2019-12-12 00:26:47 -0800

    fix bug

--- inicarBase.sh
+++ inicarBase.sh
@@ -2,5 +2,5 @@ mysqladmin -h localhost -u root -p drop ResideTec
 mysqladmin -h localhost -u root -p create ResideTec
 python manage.py makemigrations
 python manage.py migrate
-python manage.py loaddata > ResideTec/seeds.json
+python manage.py loaddata ResideTec/seeds.json
 #echo "from superusuario.models import User; User.objects.create_superuser('super@residetec.com', 'super', '123tamarindo')" | python manage.py shell
